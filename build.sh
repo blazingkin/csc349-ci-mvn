5abb4d30da56338706ba5975958a0cd001fe07e2
blazingkin <blazingkin@gmail.com>
2018-10-03 15:02:36 -0700

    Add build script

---
+++ build.sh
@@ -0,0 +1,5 @@
+mvn sonar:sonar \
+	  -Dsonar.projectKey=blazingkin_csc349ci \
+	    -Dsonar.organization=blazingkin-github \
+	      -Dsonar.host.url=https://sonarcloud.io \
+	        -Dsonar.login=$SONAR_KEY
